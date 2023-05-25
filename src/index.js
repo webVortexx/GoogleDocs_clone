746f608c76650baeae19308ae0a923794130863b
webVortexx <134393958+webVortexx@users.noreply.github.com>
2023-05-25 11:12:40 +0530

    Minor fixes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import { ThemeProvider } from "@material-tailwind/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(
   <React.StrictMode>
     <ThemeProvider>
